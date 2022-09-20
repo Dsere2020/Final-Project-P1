efa73b48177ae01811cf644cd490aa1d88ed324f
Dsere2020 <dsere2020@gmail.com>
2022-09-20 05:10:51 +0400

    first commit

---
+++ js/apple.js
@@ -0,0 +1,45 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
+
+
+
+fetch('https://bitbucket.org/hpstore/spacex-cargo-planner/raw/204125d74487b1423bbf0453f4dcb53a2161353b/shipments.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+    function appendData(data) {
+    var mainContainer = document.getElementById("appleInput");
+        mainContainer.value = data[2].boxes;
+        resMain = mainContainer.value;
+        let createArray = resMain.split(",", resMain.length);
+        console.log(createArray);  
+        const arrOfNum = createArray.map(str =>{ //Array of Numbers
+            return Number(str)
+        });
+        console.log(arrOfNum);
+        const sum = arrOfNum.reduce((acc,val) =>{
+            return acc + val;
+        }, 0);
+        const sumPrint = Math.ceil(sum/10);
+        console.log(sumPrint);
+
+        document.getElementById('resultP').innerText = sumPrint;
+
+    };
